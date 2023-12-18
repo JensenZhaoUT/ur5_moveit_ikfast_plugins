1626a806103a5e980c6821a8ce2cfd7726844d5e
JensenzhaoUT <Jensen.zhao@mail.utoronto.ca>
2023-12-18 17:22:25 -0500

    1st success

--- update_ikfast_plugin.sh
+++ update_ikfast_plugin.sh
@@ -1,13 +1,13 @@
 search_mode=OPTIMIZE_MAX_JOINT
 srdf_filename=ur5.srdf
-robot_name_in_srdf=ur5
+robot_name_in_srdf=ur5_robot
 moveit_config_pkg=ur5_moveit_config
 robot_name=ur5
 planning_group_name=manipulator
 ikfast_plugin_pkg=ur5_moveit_ikfast_plugin
 base_link_name=base_link
-eef_link_name=flange
-ikfast_output_path=/ur_ws/src/ur5_moveit_ikfast_plugins/ur5_moveit_ikfast_plugin/src/ur5_manipulator_ikfast_solver.cpp
+eef_link_name=tool0
+ikfast_output_path=/catkin_ws/src/universal_robot/ur5_moveit_ikfast_plugin/src/ur5_manipulator_ikfast_solver.cpp
 
 rosrun moveit_kinematics create_ikfast_moveit_plugin.py\
   --search_mode=$search_mode\
